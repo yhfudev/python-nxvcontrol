1633799686aecd654aea544aba5c8eb8a6b896ec
yhfudev <yhfudev@gmail.com>
2016-12-08 13:41:50 -0500

    serialbatt.py supports verious api

--- serialbatt.py
+++ serialbatt.py
@@ -5,7 +5,8 @@ import time
 import argparse # argparse > optparse > getopt
 import logging as L
 from datetime import datetime
-import serial  # sudo apt-get install python3-serial
+
+import neatocmdapi
 
 #L.basicConfig(filename='logserial.txt', level=L.DEBUG, format='%(asctime)s %(levelname)s:%(message)s')
 ch = L.StreamHandler(sys.stderr)
@@ -17,8 +18,9 @@ parser=argparse.ArgumentParser(description='Log battery data from USB port(seria
 #parser.add_argument('intergers', metavar='N', type=int, nargs='+', help='an integer for the accumulator')
 parser.add_argument('-l', '--logfile', type=str, dest='fnlog', default="/dev/stderr", help='the file to output the log')
 parser.add_argument('-o', '--output', type=str, dest='fnout', default="/dev/stdout", help='the file to output the data')
+parser.add_argument('-a', '--tcpaddr', type=str, dest='tcpaddr', default="", help='the ip address for TCP connection, use value "simulator" for simulation')
 parser.add_argument('-b', '--baudrate', type=int, dest='baudrate', default=115200, help='the baudrate of serial port')
-parser.add_argument('-p', '--port', type=str, dest='port', default="/dev/ttyACM0", help='the path to serial port')
+parser.add_argument('-p', '--port', type=str, dest='port', default="/dev/ttyACM0", help='the tcp port or the path to serial port')
 parser.add_argument('-i', '--interval', type=int, dest='interval', default=0.5, help='the interval between log records')
 parser.add_argument('-t', '--time', type=int, dest='time', default=0, help='the second time length of log records')
 parser.add_argument('-d', '--drain', type=int, dest='draintime', default=0, help='the second time length of draining the battery(start fan motor)')
@@ -34,61 +36,53 @@ if args.fnlog == "/dev/stderr":
 else:
     L.basicConfig(filename=args.fnlog, level=L.DEBUG, format='%(asctime)s %(levelname)s:%(message)s')
 
-ser=serial.Serial()
-ser.port = args.port
-ser.baudrate = args.baudrate
-ser.bytesize = serial.EIGHTBITS
-ser.parity = serial.PARITY_NONE
-ser.stopbits = serial.STOPBITS_ONE
-ser.timeout = args.interval
-#ser.xonxoff= False
-#ser.rtscts = False
-#ser.dsrdtr = False
-ser.write_timeout = 2
+api = None
+if "simulator" == args.tcpaddr.strip():
+    L.debug('NCISimulator')
+    api = neatocmdapi.NCISimulator()
+elif "" != args.tcpaddr.strip():
+    L.debug('NCINetwork')
+    api = neatocmdapi.NCINetwork(timeout = args.interval, port = int(args.port), address=args.tcpaddr)
+else:
+    L.debug('NCISerial')
+    api = neatocmdapi.NCISerial(timeout = args.interval, port = args.port, baudrate = args.baudrate)
 
-#debug
-if 0 == 1:
-    tm_begin = datetime.now()
-    time.sleep(3)
-    tm_now = datetime.now()
-    delta = tm_now - tm_begin
-    print ("second=" + str(delta.seconds))
-    print ("milli second=%d.%06d"%(delta.seconds, delta.microseconds))
-    print ("delta: " + str(delta))
-    exit()
+L.debug('api=' + str(api))
 
+if None == api:
+    L.error ("Please specify the argument for connections")
+    exit(0)
 
-try:
-    ser.open()
-except Exception as e:
-    L.error("Error open serial port: " + str(e))
-    exit()
+L.debug('api.open() ...')
+api.open()
 
-if ser.isOpen():
+time.sleep(1)
+
+cnt=1
+while api.ready() == False and cnt < 2:
+    time.sleep(1)
+    cnt += 1
+
+if api.ready() == False:
+    L.error ('time out for connection')
+    exit(0)
+
+if api.ready():
     try:
-        ser.flushInput()
-        ser.flushOutput()
+        #L.debug('api.flush() ...')
+        api.flush()
 
         # clean the input/output
-        ser.write("Help\n".encode('ASCII'))
-        ser.flush()
-        while True:
-            response = ser.readline()
-            if len(response) < 1:
-                L.debug('read null')
-                break
-            response = response.decode('ASCII').strip()
-            if len(response) < 1:
-                break
+        L.debug('api.put() ...')
+        api.put("Help")
+        L.debug('api.get() ...')
+        api.get()
+        L.debug('api.get() end')
 
         if args.draintime > 0:
-            ser.write("TestMode On\n".encode('ASCII'))
-            ser.write("SetMotor VacuumOn\n".encode('ASCII'))
-            ser.flush()
+            api.put("TestMode On\nSetMotor VacuumOn")
             time.sleep(args.draintime)
-            ser.write("SetMotor VacuumOff\n".encode('ASCII'))
-            ser.write("TestMode Off\n".encode('ASCII'))
-            ser.flush()
+            api.put("SetMotor VacuumOff\nTestMode Off")
 
         list_commands = (
             'GetCharger',
@@ -118,34 +112,24 @@ if ser.isOpen():
                     L.debug("exceed time!")
                     break
 
+            data = dict()
+            sendcmd = ""
             for cmd0 in list_commands:
                 L.debug ('send command ' + cmd0)
-                sendcmd = cmd0 + "\n"
-                ser.write(sendcmd.encode('ASCII'))
-            ser.flush()
+                sendcmd += cmd0 + "\n"
+            api.put(sendcmd)
 
-            data = dict()
             tm_now = datetime.now()
             record = [-1] * 16
-            while True:
-                try:
-                    L.debug('readline ...')
-                    response = ser.readline()
-                except TimeoutError:
-                    L.debug('timeout read')
-                    break
-                if len(response) < 1:
-                    L.debug('read null')
-                    break
-                response = response.decode('ASCII').strip()
+            retlines = api.get()
+            responses = retlines.split('\n')
+            for i in range(0,len(responses)):
+                response = responses[i].strip()
                 L.debug('received: ' + response)
                 #L.debug('read size=' + len(response) )
                 if len(response) < 1:
                     L.debug('read null 2')
                     break
-                if len(response) == 1:
-                    if response[0] == '\x1A':
-                        break
                 lst = response.split(',')
                 #L.debug('lst=' + ",".join(lst))
                 #L.debug('lst size=%d'%lst.__len__())
@@ -174,12 +158,17 @@ if ser.isOpen():
             print (fmt0 %(delta.days * 86400 + delta.seconds, delta.microseconds)) # (24*60*60)=86400
             sys.stdout.flush()
 
-        ser.close()
+            tm_now2 = datetime.now()
+            delta = tm_now2 - tm_now
+            timepast = 1.0 * delta.microseconds / 1000000
+            timepast += delta.days * 86400 + delta.seconds
+            if timepast < args.interval:
+                time.sleep(args.interval - timepast)
+
+        api.close()
 
     except Exception as e1:
         L.error ('Error in read serial: ' + str(e1))
 
 else:
     L.error('Cannot open serial port')
-
-
