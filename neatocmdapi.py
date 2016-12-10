2132db9b84f2113f2b7836f28dfae77274143388
yhfudev <yhfudev@gmail.com>
2016-12-10 10:06:28 -0500

    working AtomTaskScheduler

--- neatocmdapi.py
+++ neatocmdapi.py
@@ -2,6 +2,7 @@
 
 import time
 import logging as L
+L.basicConfig(filename='neatocmdapi.log', level=L.DEBUG, format='%(asctime)s %(levelname)s: %(message)s')
 
 class NeatoCommandInterface(object):
     "Neato Command Interface abstraction interface"
@@ -69,7 +70,7 @@ class NCISerial(NeatoCommandInterface):
 
     def close(self):
         self.isready = False
-        ser.close()
+        self.ser.close()
 
     def ready(self):
         if self.isready:
@@ -150,11 +151,11 @@ class NCINetwork(NeatoCommandInterface):
 
     def get(self):
         BUFFER_SIZE = 4096
-        MAXIUM_SIZE = BUFFER_SIZE * 5
+        #MAXIUM_SIZE = BUFFER_SIZE * 5
 
         cli_log_head = "CLI2SVR" + str(self.sock.getpeername())
         response=""
-        while 1:
+        while True:
             try:
                 recvdat = self.sock.recv(BUFFER_SIZE)
             except socket.timeout:
@@ -177,4 +178,360 @@ class NCINetwork(NeatoCommandInterface):
             L.debug('split and merge: ' + '\n'.join(requests[0:-2]) + "\n" + requests[-1])
         L.debug("get() return: " + response + self.data + "\n")
         return response + self.data + "\n"
+
+
+from multiprocessing import Queue, Lock
+import threading
+import heapq
+from datetime import datetime
+from itertools import count
+
+class MyHeap(object):
+    def __init__(self, initial=None, key=lambda x:x):
+        self._counter = count()
+        self.key = key
+        if initial:
+            self._data = [(key(item), next(self._counter), item) for item in initial]
+            heapq.heapify(self._data)
+        else:
+            self._data = []
+
+    def size(self):
+        return len(self._data)
+
+    def push(self, item):
+        heapq.heappush(self._data, (self.key(item), next(self._counter), item))
+
+    def pop(self):
+        return heapq.heappop(self._data)[-1]
+
+# each task will executed until finished
+# supports priority, 0 -- critial for important task, 1-n -- normal priority tasks
+# supports execute at a exact time.
+class AtomTaskScheduler(object):
+
+    # internal class
+    class AtomTask(object):
+        PRIORITY_DEFAULT = 5
+        PRIORITY_MAX=255
+
+        def __init__(self, req=None, newid=0, priority=None):
+            self.req = req # user define
+            self.tid = newid
+            self.priority = priority
+            self.execute_time = None
+            self.request_time = None
+            self.start_time = None
+            self.finish_time = None
+
+        def setPriority(self, pri):
+            self.priority = pri
+
+        def setRequestTime(self, etime):
+            self.request_time = etime
+
+        def setExecuteTime(self, etime):
+            self.execute_time = etime
+
+        def setStartTime(self, etime):
+            self.start_time = etime
+
+        def setFinishTime(self, etime):
+            self.finish_time = etime
+
+    # use two queues to accept and cache the requests,
+    #  queue_priority is for tasks with priority
+    #  queue_time is for tasks with exact time
+    #
+    # loop:
+    #  move tasks from queue_priority to heap_priority
+    #  move tasks from queue_time to heap_time
+    #  if heap_time has expired tasks need to execute, move the tasks(priority=1) to heap_priority
+    #  if heap_priority has task, do the task, remove it from heap_priority, return results(id, request time, begin time, finish time, response)
+    #  if has do task, goto loop
+    #  wait_time=1
+    #  if has task in heap_time, wait_time = wait time for the top task
+    #  cond_wait(wait_time)
+    def __init__(self, cb_task=None, cb_signal=None):
+        self.cb_task = cb_task
+        self.cb_signal = cb_signal
+        self.queue_priority = Queue()
+        self.queue_time = Queue()
+        self.heap_priority = MyHeap(key=lambda x:x.priority);
+        self.heap_time = MyHeap(key=lambda x:x.execute_time);
+
+        self._counter = count()
+        self.idlock = Lock()
+        self.condnewtask= threading.Condition()
+
+    # create a new request id
+    def getNewId(self):
+        self.idlock.acquire()
+        try:
+            return next(self._counter)
+        finally:
+            self.idlock.release()
+
+    # request for a task, with the priority
+    def request(self, req, priority):
+        newid = self.getNewId()
+        newreq = AtomTaskScheduler.AtomTask(req=req, newid=newid, priority=priority)
+        newreq.setRequestTime(datetime.now())
+        self.queue_priority.put(newreq)
+        with self.condnewtask:
+            self.condnewtask.notifyAll()
+        return newid
+
+    # request for a task, with the exact time
+    def request_time (self, req, exacttime):
+        newid = self.getNewId()
+        newreq = AtomTaskScheduler.AtomTask(req=req, newid=newid)
+        newreq.setRequestTime(datetime.now())
+        newreq.setExecuteTime(exacttime)
+        self.queue_time.put(newreq)
+        with self.condnewtask:
+            self.condnewtask.notifyAll()
+        return newid
+
+    def do_work_once (self):
+        # move tasks from queue_priority to heap_priority
+        while self.queue_priority.qsize() > 0:
+            newreq = None
+            try:
+                newreq = self.queue_priority.get()
+                L.debug("get req from queue_priority: " + str(newreq))
+            except Queue.Empty:
+                break;
+            self.heap_priority.push (newreq)
+        # move tasks from queue_time to heap_time
+        while self.queue_time.qsize() > 0:
+            newreq = None
+            try:
+                newreq = self.queue_time.get()
+            except Queue.Empty:
+                break;
+            self.heap_time.push (newreq)
+        # if heap_time has expired tasks need to execute, move the tasks(priority=1) to heap_priority
+        wait_time=0 # seconds, wait time for cond
+        while (self.heap_time.size() > 0):
+            newreq = self.heap_time.pop()
+            tmnow = datetime.now()
+            if newreq.execute_time <= tmnow:
+                newreq.setPriority(1)
+                self.heap_priority.push(newreq)
+            else:
+                # push back the item
+                delta = newreq.execute_time - tmnow
+                wait_time = delta.days * 86400 + delta.seconds + delta.microseconds/1000000
+                self.heap_time.push(newreq)
+        # if heap_priority has task, do the task, remove it from heap_priority, return results(id, request time, begin time, finish time, response)
+        if (self.heap_priority.size() > 0):
+            newreq = self.heap_priority.pop()
+            newreq.setStartTime(datetime.now())
+            resp = self.cb_task (newreq.req)    # do the job
+            newreq.setFinishTime(datetime.now())
+            self.cb_signal (newreq.tid, newreq.request_time, newreq.start_time, newreq.finish_time, resp) # callback, return result
+            #return True # if has done task, goto loop
+        return wait_time
+
+    def do_wait_queue(self, wait_time):
+        # if has task in heap_time, wait_time = wait time for the top task
+        with self.condnewtask:
+            try:
+                L.debug("waiting for queues ...")
+                self.condnewtask.wait(wait_time)
+            except RuntimeError:
+                L.debug("wait timeout!")
+                time.sleep(0.01)
+
+    def serve_forever (self):
+        while True:
+            wait_time = self.do_work_once()
+            if wait_time > 0:
+                self.do_wait_queue(wait_time)
+
+#from urlparse import urlparse
+from urllib.parse import urlparse
+import neatocmdapi
+class NCIService(object):
+    "Neato Command Interface all"
+    def cb_task1(self, req):
+        L.debug("do task: req=" + str(req))
+    def cb_signal1(self, tid, request_time, start_time, finish_time, resp):
+        L.debug("signal: tid=" + str(tid) + ", reqtime=" + str(request_time) + ", starttime=" + str(start_time) + ", fintime=" + str(finish_time) + ", resp=" + str(resp) )
+
+    def __init__(self, target="tcp://localhost:3333", timeout=1):
+        "target accepts: 'tcp://localhost:3333', '/dev/ttyUSB0', 'sim:' "
+        self.api = None
+        result = urlparse(target)
+        if result.scheme == "tcp":
+            addr = result.netloc.split(':')
+            port = 3333
+            if len(addr) > 1:
+                port = int(addr[1])
+            self.api = neatocmdapi.NCINetwork(timeout = timeout, port = port, address=addr[0])
+
+        elif result.scheme == "sim":
+            self.api = neatocmdapi.NCISimulator()
+
+        else:
+            baudrate = 115200
+            self.api = neatocmdapi.NCISerial(timeout = timeout, port = result.path, baudrate = baudrate)
+
+        #if None == self.api:
+
+def test_nci_service():
+    a = NCIService()
+
+
+
+def test_heap_priority():
+    hp = MyHeap()
+    hp.push (5)
+    hp.push (3)
+    hp.push (2)
+    hp.push (0)
+    hp.push (6)
+    hp.push (4)
+    hp.push (1)
+    tmpre = hp.pop()
+    while hp.size() > 0:
+        tm = hp.pop()
+        assert (tm > tmpre);
+        tmpre = tm
+
+def test_heap_time():
+    hp = MyHeap()
+    tm = datetime.now()
+    hp.push(tm)
+    time.sleep(0.01)
+    tm = datetime.now()
+    hp.push(tm)
+    time.sleep(0.31)
+    tm = datetime.now()
+    hp.push(tm)
+    tmpre = hp.pop()
+    while hp.size() > 0:
+        tm = hp.pop()
+        assert (tm > tmpre);
+        tmpre = tm
+
+def test_heap_atomtask_priority():
+    hp = MyHeap(key=lambda x:x.priority);
+    newreq = AtomTaskScheduler.AtomTask(req="req4", priority=4)
+    hp.push (newreq)
+    newreq = AtomTaskScheduler.AtomTask(req="req2", priority=2)
+    hp.push (newreq)
+    newreq = AtomTaskScheduler.AtomTask(req="req1", priority=1)
+    hp.push (newreq)
+    newreq = AtomTaskScheduler.AtomTask(req="req0", priority=0)
+    hp.push (newreq)
+    newreq = AtomTaskScheduler.AtomTask(req="req5", priority=5)
+    hp.push (newreq)
+    newreq = AtomTaskScheduler.AtomTask(req="req3", priority=3)
+    hp.push (newreq)
+    tmpre = hp.pop()
+    while hp.size() > 0:
+        tm = hp.pop()
+        assert (tm.priority > tmpre.priority);
+        tmpre = tm
+
+def test_heap_atomtask_time():
+    hp = MyHeap(key=lambda x:x.execute_time);
+    newreq = AtomTaskScheduler.AtomTask(req="req1")
+    newreq.setExecuteTime(datetime.now())
+    hp.push(newreq)
+    time.sleep(0.01)
+    newreq = AtomTaskScheduler.AtomTask(req="req2")
+    newreq.setExecuteTime(datetime.now())
+    hp.push(newreq)
+    time.sleep(0.31)
+    tm = datetime.now()
+    newreq = AtomTaskScheduler.AtomTask(req="req3")
+    newreq.setExecuteTime(datetime.now())
+    hp.push(newreq)
+
+    tmpre = hp.pop()
+    while hp.size() > 0:
+        tm = hp.pop()
+        assert (tm.execute_time > tmpre.execute_time);
+        tmpre = tm
+
+
+def test_heap_atomtask_priority_class():
+    class TestContainer(object):
+        def __init__(self):
+            self.hp = MyHeap(key=lambda x:x.priority);
+        def selftest(self):
+            newreq = AtomTaskScheduler.AtomTask(req="req4", priority=4)
+            self.hp.push (newreq)
+            newreq = AtomTaskScheduler.AtomTask(req="req2", priority=2)
+            self.hp.push (newreq)
+            newreq = AtomTaskScheduler.AtomTask(req="req1", priority=1)
+            self.hp.push (newreq)
+            newreq = AtomTaskScheduler.AtomTask(req="req0", priority=0)
+            self.hp.push (newreq)
+            newreq = AtomTaskScheduler.AtomTask(req="req5", priority=5)
+            self.hp.push (newreq)
+            newreq = AtomTaskScheduler.AtomTask(req="req3", priority=3)
+            self.hp.push (newreq)
+            tmpre = self.hp.pop()
+            while self.hp.size() > 0:
+                tm = self.hp.pop()
+                assert (tm.priority >= tmpre.priority);
+                assert (tm.priority >= tmpre.priority);
+                tmpre = tm
+    tc = TestContainer()
+    tc.selftest()
+
+def test_atomtask():
+    def cb_task1(req):
+        L.debug("do task: req=" + str(req))
+    def cb_signal1(tid, request_time, start_time, finish_time, resp):
+        L.debug("signal: tid=" + str(tid) + ", reqtime=" + str(request_time) + ", starttime=" + str(start_time) + ", fintime=" + str(finish_time) + ", resp=" + str(resp) )
+
+    def th_setup(sche):
+        sche.request("init work 2-1", 2)
+        sche.request("init work 2-3", 2)
+        sche.request("init work 1-2", 1)
+        sche.request("init work 2-2", 2)
+        sche.request("init work 0-1", 0)
+        sche.request("init work 1-1", 1)
+        sche.request("init work 2-4", 2)
+        sche.request("init work 0-2", 0)
+        time.sleep(5)
+        sche.request("normal work 5-1", 5)
+        sche.request("normal work 5-2", 5)
+        sche.request("normal work 5-3", 5)
+        sche.request("normal work 5-4", 5)
+
+    sche = AtomTaskScheduler(cb_task=cb_task1, cb_signal=cb_signal1)
+
+    if 1 == 1:
+        runT = threading.Thread(target=sche.serve_forever)
+        runT.setDaemon(True)
+        runT.start()
+        th_setup(sche)
+        L.debug("sleep ...")
+        time.sleep(5.31)
+    else:
+        runT = threading.Thread(target=th_setup, args=(sche,))
+        runT.setDaemon(True)
+        runT.start()
+        sche.serve_forever()
+
+def testme():
+    test_heap_time()
+    test_heap_priority()
+    test_heap_atomtask_priority()
+    test_heap_atomtask_time()
+    test_heap_atomtask_priority_class()
+    test_atomtask()
+    #test_nci_service()
+
+
+if __name__ == "__main__":
+    testme()
+
+
 
