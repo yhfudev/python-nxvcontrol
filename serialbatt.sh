e0fa1c6275d22800aefaef721dbc5678e5d97359
yhfudev <yhfudev@gmail.com>
2016-12-28 14:58:13 -0500

    add script

---
+++ serialbatt.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+#python3 serialbatt.py -l serialbatt.log -o serialbatt-data-charging-powerextra-r1-m1-1.txt -a dev://ttyACM0:115200
+python3 serialbatt.py -l serialbatt.log -o serialbatt-data-charging-oem-m2-xx.txt -a dev://ttyACM0:115200
