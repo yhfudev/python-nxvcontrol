74856351a33b4011a5f72a79a7ab2446c64bc8ab
yhfudev <yhfudev@gmail.com>
2016-12-28 15:04:31 -0500

    update the script

--- serialbatt.bat
+++ serialbatt.bat
@@ -1,4 +1,3 @@
-rem python serialbatt.py -o serialbatt-data-charging-oem-m2-1.txt -l log.txt -p COM12
 
-python serialbatt.py -o serialbatt-data-charging-powerextra-r1-m1-1.txt -l log.txt -p COM12
-
+rem python serialbatt.py -l serialbatt.log -o serialbatt-data-charging-oem-m2-1.txt -a dev://COM12:115200
+python serialbatt.py -l serialbatt.log -o serialbatt-data-charging-powerextra-r1-m1-1.txt -a dev://COM12:115200
