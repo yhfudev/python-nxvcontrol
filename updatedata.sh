d04292a9a964bd25e4ca52d13879e2ad204f5ee7
yhfudev <yhfudev@gmail.com>
2016-12-29 07:47:44 -0500

    change the name to nxvlogbatt

--- updatedata.sh
+++ updatedata.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-FN_DATA="serialbatt-data-charging-oem-m2-1.txt serialbatt-data-charging-oem-m2-2.txt serialbatt-data-charging-powerextra-r1-m1-1.txt"
-FN_DATA="serialbatt-data-charging-oem-m2-3.txt"
+FN_DATA="nxvlogbatt-data-charging-oem-m2-1.txt nxvlogbatt-data-charging-oem-m2-2.txt nxvlogbatt-data-charging-powerextra-r1-m1-1.txt"
+FN_DATA="nxvlogbatt-data-charging-oem-m2-3.txt"
 
 # pacman -S inotify-tools gnuplot
 # apt-get install inotify-tools gnuplot
 if inotifywait -e modify ${FN_DATA}; then
     echo "plotting ..."
     date
-    ./serialbatt-plotfig.sh
+    ./nxvlogbatt-plotfig.sh
     date
     echo "ploting done!"
 fi
