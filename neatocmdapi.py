d87faa9113f9896b50cc7cdcad888c357f94bc63
yhfudev <yhfudev@gmail.com>
2017-08-23 11:53:09 -0400

    add copyright messages

--- neatocmdapi.py
+++ neatocmdapi.py
@@ -1,4 +1,21 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+# encoding: utf8
+#
+# Copyright 2016-2017 Yunhui Fu <yhfudev@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3, as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranties of
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# For further info, check https://github.com/yhfudev/python-nxvcontrol.git
 
 import time
 import logging as L
