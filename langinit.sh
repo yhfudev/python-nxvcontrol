671e46b8265abd052a17e824666b9164c1795caa
yhfudev <yhfudev@gmail.com>
2017-09-14 11:26:59 -0400

    update the language script

--- langinit.sh
+++ langinit.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-# init languages
-
-FN_TEMPLATE="nxvcontrol.pot"
+# initialize transloation files of international languages
 
 parse_pyfile() {
-    PARAM_LANGS=$1
+    local PARAM_PREFIX_OUTMO=$1
     shift
-    PARAM_FILES=$1
+    local PARAM_LANGS=$1
     shift
+    local PARAM_FILES=$1
+    shift
+
+    local FN_TEMPLATE="${PARAM_PREFIX_OUTMO}.pot"
 
     if [ ! -f "${FN_TEMPLATE}" ]; then
         # xgettext --language=Python --keyword=_ --from-code utf-8 --output=nxvcontrol.pot nxvforward.py
@@ -15,6 +17,7 @@ parse_pyfile() {
     else
         pygettext3 --output=tmp.pot ${PARAM_FILES}
         cat tmp.pot | egrep "msgid |msgstr |^$" | grep -v 'msgid ""\nmsgstr ""' >> "${FN_TEMPLATE}"
+        rm -f tmp.pot
     fi
 
     mkdir -p translations/
@@ -25,11 +28,10 @@ parse_pyfile() {
         msgmerge --update translations/${i}.po "${FN_TEMPLATE}"
 
         mkdir -p languages/${i}/LC_MESSAGES/
-        msgfmt --output-file=languages/${i}/LC_MESSAGES/nxvcontrol.mo translations/${i}.po
+        msgfmt --output-file="languages/${i}/LC_MESSAGES/${PARAM_PREFIX_OUTMO}.mo" "translations/${i}.po"
     done
 }
 
-rm -f "${FN_TEMPLATE}"
-
-parse_pyfile "en_US zh_CN" "nxvforward.py nxvcontrol.py"
+rm -f "nxvcontrol.pot"
+parse_pyfile "nxvcontrol" "en_US zh_CN" "nxvforward.py nxvcontrol.py"
 
